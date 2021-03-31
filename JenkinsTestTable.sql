739b6acf8035f56e000953401e604ef28134660e
cwillis13 <62621035+cwillis13@users.noreply.github.com>
2021-03-31 17:40:25 -0400

    Create JenkinsTestTable.sql

---
+++ JenkinsTestTable.sql
@@ -0,0 +1,25 @@
+USE [BrimJenkins]
+GO
+
+/****** Object:  Table [dbo].[JenkinsTestTable]    Script Date: 3/29/2021 3:14:01 PM ******/
+SET ANSI_NULLS OFF
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[JenkinsTestTable](
+	[VehicleTypeID] [int] IDENTITY(1,1) NOT NULL,
+	[SiteID] [int] NOT NULL,
+	[VehicleTypeDescription] [varchar](20) NOT NULL,
+	[VehicleTypeGroupID] [int] NOT NULL,
+	[IsActive] [bit] NOT NULL,
+	[LastUpdate] [datetime] NULL,
+	[LastUserID] [int] NULL,
+	[RowGUID] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+ CONSTRAINT [PK_JenkinsTestTable] PRIMARY KEY NONCLUSTERED 
+(
+	[VehicleTypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80) ON [PRIMARY]
+) ON [PRIMARY]
+GO
