2ef30168b9fb3ee1065b6ec83cb4fe36684353df
cwillis13 <62621035+cwillis13@users.noreply.github.com>
2021-04-01 14:33:25 -0400

    Update deploy_scripts.sh

--- deploy_scripts.sh
+++ deploy_scripts.sh
@@ -4,7 +4,10 @@ while read file
 
 do
 
-        echo "sqlcmd -U yxgrr -P password -S sql_instance -d Brim_Jenkins -i $file -e >> ${file}.out"
-        echo "Checking output file ${file}.out"
+        #echo "sqlcmd -U yxgrr -P password -S sql_instance -d Brim_Jenkins -i $file -e >> ${file}.out"
+        #echo "Checking output file ${file}.out"
+        echo "here's some sample output for $file execution" > ${file}.out
+        echo "catting ${file}.out below"
+        cat ${file}.out
 
 done < $scripts
