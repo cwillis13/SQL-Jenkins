a5e7090a10703c6e6a32202b63f86b68d7290935
cwillis13 <62621035+cwillis13@users.noreply.github.com>
2021-04-01 16:57:35 -0400

    Update deploy_scripts.sh

--- deploy_scripts.sh
+++ deploy_scripts.sh
@@ -2,17 +2,17 @@ cat ${INI} | while read line
 do
    #     echo "sqlcmd -U ${SQLUSERNAME} -P ${sqlpass} -S ${SQLINST} -d ${DBNAME} -i JenkinsTestTable.sql -e > JenkinsTestTable_load_exec_log_FIXED.txt"
         echo "Running script ${line}......"
-        echo "----------------------------------------------------------------------------"
+        echo "----------------------------------------------------------------------------------------------"
         sqlcmd -U ${SQLUSERNAME} -P ${SQLPASS} -S ${SQLINST} -d ${DBNAME} -i ${line} -e > ${line}.out
         
         error=`grep 'Level[[:blank:]][0-9][0-9]' ${line}.out`
 
         if [[ $error ]]; then
-                echo "There was an error after executing the SQL script... EXITING!..."
-                echo "----------------------------------------------------------------------------"
+                echo "There was an error after executing the SQL script. View ${line}.out for details... EXITING!..."
+                echo "----------------------------------------------------------------------------------------------"
                 exit 1
         else
                 echo "No errors... Moving on..."
-                echo "----------------------------------------------------------------------------"
+                echo "----------------------------------------------------------------------------------------------"
         fi
 done
