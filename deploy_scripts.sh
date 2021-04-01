a493322c7fd33bf7b31a9822fd3beb25d5b9fa9f
cwillis13 <62621035+cwillis13@users.noreply.github.com>
2021-04-01 14:37:23 -0400

    Update deploy_scripts.sh

--- deploy_scripts.sh
+++ deploy_scripts.sh
@@ -6,8 +6,6 @@ do
 
         #echo "sqlcmd -U yxgrr -P password -S sql_instance -d Brim_Jenkins -i $file -e >> ${file}.out"
         #echo "Checking output file ${file}.out"
-        echo "here's some sample output for $file execution" > ${file}.out
-        echo "catting ${file}.out below"
-        cat ${file}.out
+        sqlcmd -U ${SQLUSERNAME} -P ${sqlpass} -S ${SQLINST} -d ${DBNAME} -i $file -e > JenkinsTestTable_load_exec_log_FIXED.txt
 
 done < $scripts
