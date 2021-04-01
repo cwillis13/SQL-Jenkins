d9f50d638d93c1ee8bcf241dffe5f2ca2e176421
cwillis13 <62621035+cwillis13@users.noreply.github.com>
2021-04-01 14:57:39 -0400

    Update deploy_scripts.sh

--- deploy_scripts.sh
+++ deploy_scripts.sh
@@ -14,5 +14,5 @@ scripts=$1
 cat $scripts | while read line 
 do
         echo "sqlcmd -U ${SQLUSERNAME} -P ${sqlpass} -S ${SQLINST} -d ${DBNAME} -i JenkinsTestTable.sql -e > JenkinsTestTable_load_exec_log_FIXED.txt"
-        sqlcmd -U ${SQLUSERNAME} -P ${sqlpass} -S ${SQLINST} -d ${DBNAME} -i $file -e > JenkinsTestTable_load_exec_log_FIXED.txt
+        sqlcmd -U ${SQLUSERNAME} -P ${sqlpass} -S ${SQLINST} -d ${DBNAME} -i "$file" -e > JenkinsTestTable_load_exec_log_FIXED.txt
 done
