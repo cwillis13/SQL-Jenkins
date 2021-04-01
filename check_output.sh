4ba32f7f05e43d9502e0ad73948fb0fc2e20f59b
cwillis13 <62621035+cwillis13@users.noreply.github.com>
2021-04-01 16:44:15 -0400

    Delete check_output.sh

--- check_output.sh
+++
@@ -1,9 +0,0 @@
-file=$1
-
-error=`grep 'Level[[:blank:]][0-9][0-9]' file`
-
-if [[ $error ]]; then
-        echo "There was an error after executing the SQL script... EXITING!..."
-else
-        echo "No errors... Moving to next script execution..."
-fi
