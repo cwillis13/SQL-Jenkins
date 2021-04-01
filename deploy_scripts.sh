ede55e2b999968faa213621a65127d2ffee1b100
cwillis13 <62621035+cwillis13@users.noreply.github.com>
2021-04-01 14:59:22 -0400

    Update deploy_scripts.sh

--- deploy_scripts.sh
+++ deploy_scripts.sh
@@ -1,18 +1,18 @@
 scripts=$1
 
-#while read file
+while read file
 
-#do
+do
 
         #echo "sqlcmd -U yxgrr -P password -S sql_instance -d Brim_Jenkins -i $file -e >> ${file}.out"
         #echo "Checking output file ${file}.out"
-  #      echo "sqlcmd -U ${SQLUSERNAME} -P ${sqlpass} -S ${SQLINST} -d ${DBNAME} -i JenkinsTestTable.sql -e > JenkinsTestTable_load_exec_log_FIXED.txt"
-   #     sqlcmd -U ${SQLUSERNAME} -P ${sqlpass} -S ${SQLINST} -d ${DBNAME} -i $file -e > JenkinsTestTable_load_exec_log_FIXED.txt
-
-#done < $scripts
-
-cat $scripts | while read line 
-do
         echo "sqlcmd -U ${SQLUSERNAME} -P ${sqlpass} -S ${SQLINST} -d ${DBNAME} -i JenkinsTestTable.sql -e > JenkinsTestTable_load_exec_log_FIXED.txt"
         sqlcmd -U ${SQLUSERNAME} -P ${sqlpass} -S ${SQLINST} -d ${DBNAME} -i "$file" -e > JenkinsTestTable_load_exec_log_FIXED.txt
-done
+
+done < $scripts
+
+#cat $scripts | while read line 
+#do
+   #     echo "sqlcmd -U ${SQLUSERNAME} -P ${sqlpass} -S ${SQLINST} -d ${DBNAME} -i JenkinsTestTable.sql -e > JenkinsTestTable_load_exec_log_FIXED.txt"
+    #    sqlcmd -U ${SQLUSERNAME} -P ${sqlpass} -S ${SQLINST} -d ${DBNAME} -i "$file" -e > JenkinsTestTable_load_exec_log_FIXED.txt
+#done
