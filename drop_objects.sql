e011d9343911ff7551dd46d86cf1e51c0f9c7108
cwillis13 <62621035+cwillis13@users.noreply.github.com>
2021-06-24 12:42:16 -0400

    Create drop_objects.sql

---
+++ drop_objects.sql
@@ -0,0 +1,14 @@
+USE [BrimJenkins]
+GO
+
+/****** Object:  Table [dbo].[JenkinsTestTable]    Script Date: 3/29/2021 3:14:01 PM ******/
+SET ANSI_NULLS OFF
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE [dbo].[JenkinsTestTable]
+DROP PROCEDURE [dbo].[JenkinsTestTable_load]
+
+GO
