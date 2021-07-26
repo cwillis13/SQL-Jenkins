0ee3b19d2cee465bdf3eb8df6ebfa6b1b7ee8909
cwillis13 <62621035+cwillis13@users.noreply.github.com>
2021-07-25 22:46:26 -0400

    Update deploy_scripts.sh

--- deploy_scripts.sh
+++ deploy_scripts.sh
@@ -4,12 +4,12 @@ cat ${INI} | while read line
 do
         echo "Running script ${line}......"
         echo "----------------------------------------------------------------------------------------------"
-        sqlcmd -U ${SQLUSERNAME} -P ${SQLPASS} -S ${SQLINST} -d ${DBNAME} -i ${line} -e > ${line}.out
+        sqlcmd -U ${SQLUSERNAME} -P ${SQLPASS} -S ${SQLINST} -d ${DBNAME} -i ${line} -e > ${line}.out.txt
         
-        error=`grep 'Level[[:blank:]][0-9][0-9]' ${line}.out`
+        error=`grep 'Level[[:blank:]][0-9][0-9]' ${line}.out.txt`
 
         if [[ $error ]]; then
-                echo "There was an error after executing the SQL script. View ${line}.out for details... EXITING!..."
+                echo "There was an error after executing the SQL script. View ${line}.out.txt for details... EXITING!..."
                 echo "----------------------------------------------------------------------------------------------"
                 echo ""
                 exit 1
