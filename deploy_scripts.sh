09f4b28822eabcd7993f183880884e310dac0978
cwillis13 <62621035+cwillis13@users.noreply.github.com>
2021-08-12 09:04:39 -0400

    Update deploy_scripts.sh

--- deploy_scripts.sh
+++ deploy_scripts.sh
@@ -6,12 +6,12 @@ do
         echo "----------------------------------------------------------------------------------------------"
         #Get filename only
         filename=`echo ${line} | sed -n 's/^\(.*\/\)*\(.*\)/\2/p'`
-        sqlcmd -U ${SQLUSERNAME} -P ${SQLPASS} -S ${SQLINST} -d ${DBNAME} -i ${line} -e > ./logs/${filename}.out.txt
+        sqlcmd -U ${SQLUSERNAME} -P ${SQLPASS} -S ${SQLINST} -d ${DBNAME} -i ${line} -e > ./logs/${filename}.txt
         
-        error=`grep 'Level[[:blank:]][0-9][0-9]' ./logs/${filename}.out.txt`
+        error=`grep 'Level[[:blank:]][0-9][0-9]' ./logs/${filename}.txt`
 
         if [[ $error ]]; then
-                echo "There was an error after executing the SQL script. View ./logs/${filename}.out.txt for details... EXITING!..."
+                echo "There was an error after executing the SQL script. View ./logs/${filename}.txt for details... EXITING!..."
                 echo "----------------------------------------------------------------------------------------------"
                 echo ""
                 exit 1
